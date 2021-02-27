f8530afa8c330ebf26ef65331840966fa9b69632
richardbrunson <richardbrunson@gmail.com>
2021-02-27 08:56:53 -0600

    Initial application structure
    This is just a skeleton framework to get the app running.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 const users = require('./../data/users.json');
+
+'use strict';
+
+const viewUsers = () => {
+    console.log('viewUsers() ' + Math.random());
+}
+
+const createUser = () => {
+    console.log('createUser() ' + Math.random());
+}
+
+const updateUser = () => {
+    console.log('updateUser() ' + Math.random());
+}
+
+const deleteUser = () => {
+    console.log('deleteUser() ' + Math.random());
+}
+
+$(document).ready(() => {
+    $('#view').click(() => { viewUsers() });
+    $('#create').click(() => { createUser() });
+    $('#update').click(() => { updateUser() });
+    $('#delete').click(() => { deleteUser() });
+});
