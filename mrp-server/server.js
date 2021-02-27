a4d6260e7c71f5a19bb352f14434286e7fed1a9e
richardbrunson <richardbrunson@gmail.com>
2021-02-27 09:42:30 -0600

    Added server.js file to display initial results
    A bare-bones Nodejs server file

---
+++ mrp-server/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs').promises;
+const host = 'localhost';
+const port = 8000;
+
+const mrp = (req, res) => {
+	fs.readFile('../src/index.html')
+		.then(content => {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end(content);
+		})
+		.catch(err => {
+			res.writeHead(500);
+			res.end(err);
+			return;
+		});
+};
+
+const server = http.createServer(mrp);
+server.listen(port, host, () => {
+	console.log(`MRP Server is running on http://${host}:${port}`);
+});
