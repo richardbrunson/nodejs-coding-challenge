bd22c73ea67eba9a9071e2ca4905a9b53cb44083
richardbrunson <richardbrunson@gmail.com>
2021-02-27 11:49:07 -0600

    Added User display functionality and updated stylesheets

--- mrp-server/server.js
+++ mrp-server/server.js
@@ -1,23 +1,25 @@
 const http = require('http');
+const path = require('path');
 const fs = require('fs').promises;
+const users = require('./../data/users.json');
 const host = 'localhost';
 const port = 8000;
 
 const mrp = (req, res) => {
-	fs.readFile('../src/index.html')
-		.then(content => {
-			res.setHeader('Content-Type', 'text/html');
-			res.writeHead(200);
-			res.end(content);
-		})
-		.catch(err => {
-			res.writeHead(500);
-			res.end(err);
-			return;
-		});
+    fs.readFile('../src/index.html')
+        .then(content => {
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200);
+            res.end(content);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
 };
 
 const server = http.createServer(mrp);
 server.listen(port, host, () => {
-	console.log(`MRP Server is running on http://${host}:${port}`);
+    console.log(`MRP Server is running on http://${host}:${port}`);
 });
